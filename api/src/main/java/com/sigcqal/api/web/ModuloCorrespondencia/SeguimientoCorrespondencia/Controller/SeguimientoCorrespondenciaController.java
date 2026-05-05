2c8e7e3ad81e7f25dc0fbe5b71257055337d6e25
PandAnaXvX-afk <anarosa290801@gmail.com>
2026-05-05 16:16:43 -0600

    SEGUIMIENTO CORRESPONDENCIA MODF
    Modificación en código

--- api/src/main/java/com/sigcqal/api/web/ModuloCorrespondencia/SeguimientoCorrespondencia/Controller/SeguimientoCorrespondenciaController.java
+++ api/src/main/java/com/sigcqal/api/web/ModuloCorrespondencia/SeguimientoCorrespondencia/Controller/SeguimientoCorrespondenciaController.java
@@ -3,6 +3,7 @@ package com.sigcqal.api.web.ModuloCorrespondencia.SeguimientoCorrespondencia.Con
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.sigcqal.api.application.ModuloCorrespondencia.SeguimientoCorrespondencia.SeguimientoCorrespondenciaService;
 import com.sigcqal.api.web.ModuloCorrespondencia.SeguimientoCorrespondencia.Dto.SeguimientoCorrespondenciaRequestDTO;
@@ -24,11 +26,11 @@ import lombok.RequiredArgsConstructor;
 public class SeguimientoCorrespondenciaController {
 
     private final SeguimientoCorrespondenciaService service;
-
-    @PostMapping("/guardar")
+    @PostMapping(value = "/guardar", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<SeguimientoCorrespondenciaResponseDTO> guardar(
-            @RequestBody SeguimientoCorrespondenciaRequestDTO request) {
-        return ResponseEntity.ok(service.guardar(request));
+            @ModelAttribute SeguimientoCorrespondenciaRequestDTO request) {
+        // Spring ahora sí entenderá el "FormData" del frontend
+            return ResponseEntity.ok(service.guardar(request));
     }
 
     @GetMapping("/listar")
