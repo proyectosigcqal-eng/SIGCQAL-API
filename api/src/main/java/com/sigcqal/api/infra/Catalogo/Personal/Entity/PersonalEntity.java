9947b6c1bf0c9d98bbac4cfacdcb5ab855d76a5d
PandAnaXvX-afk <anarosa290801@gmail.com>
2026-06-28 23:25:09 -0600

    AJUSTES PARA BORRADO EN CASCADA: CRUD PERSONAL
    Borrar un registro de personal junto con su id de persona asignado. DELETE probado y funcionando.

--- api/src/main/java/com/sigcqal/api/infra/Catalogo/Personal/Entity/PersonalEntity.java
+++ api/src/main/java/com/sigcqal/api/infra/Catalogo/Personal/Entity/PersonalEntity.java
@@ -20,10 +20,10 @@ public class PersonalEntity {
 
     // Relación ManyToOne: Muchas personas pueden estar en la tabla personal
     // (o OneToOne si la regla es estrictamente una persona por cada registro de personal)
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "id_persona", nullable = false)
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "id_persona")
     private PersonaEntity persona;
-
+    
     @Column(name = "fecha_registro")
     private LocalDateTime fechaRegistro;
 
