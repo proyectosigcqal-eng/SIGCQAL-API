ff1f6f5aca71f619661fd7e9371b48f52d77c4c6
JuanMerino15 <juanrivmerino4@gmail.com>
2026-07-09 18:27:23 -0600

    Corrección en usuarios y security config

--- api/src/main/java/com/sigcqal/api/config/SecurityConfig.java
+++ api/src/main/java/com/sigcqal/api/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 .requestMatchers("/api/files/**").permitAll()
                 .requestMatchers("/catalogos/areas/**")
-                    .hasAnyRole("Administrador", "Administrador_Correspondencia")
+                    .hasAnyRole("Administrador", "Administrador_Correspondencia", "Gestor de Correspondencia", "Asesor")
                 .requestMatchers("/api/admin/**").hasRole("Administrador")
                 .anyRequest().authenticated()
             )
